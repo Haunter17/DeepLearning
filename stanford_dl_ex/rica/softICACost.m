90da5fc861ac16ff34b635ce5fa5e2f16feb607b
Haunter17 <haunterhacker17@gmail.com>
2017-02-13 01:41:41 -0800

    rica

--- stanford_dl_ex/rica/softICACost.m
+++ stanford_dl_ex/rica/softICACost.m
@@ -9,7 +9,19 @@ Wold = W;
 W = l2rowscaled(W, 1);
 
 %%% YOUR CODE HERE %%%
+%% Compute Cost
+temp1 = sqrt((W * x).^ 2 + params.epsilon);
+regCost = params.lambda * sum(sum(temp1));
+temp2 = (W' * W * x - x).^ 2;
+RICACost = 0.5 * sum(sum(temp2)) / size(x, 2);
+cost = regCost + RICACost;
+%% Compute Gradient
+RICAgrad = (W * (W' * W * x - x) * x' + (W * x) * (W' * W * x - x)')...
+    / size(x, 2);
+regGrad = params.lambda * W * x ./ temp1 * x';
+Wgrad = regGrad + RICAgrad;
 
+%%
 % unproject gradient for minFunc
 grad = l2rowscaledg(Wold, W, Wgrad, 1);
 grad = grad(:);
--- stanford_dl_ex/rica/zca2.m
+++ stanford_dl_ex/rica/zca2.m
@@ -8,3 +8,30 @@ epsilon = 1e-4;
 % z is the ZCA transformed data. The dimenison of z = x.
 
 %%% YOUR CODE HERE %%%
+%% Step 0b: Zero-mean the data (by row)
+%  You can make use of the mean and repmat/bsxfun functions.
+
+%%% YOUR CODE HERE %%%
+avg = mean(x, 1);
+x = x - repmat(avg, size(x, 1), 1);
+
+%% Step 1a: Implement PCA to obtain xRot
+%  Implement PCA to obtain xRot, the matrix in which the data is expressed
+%  with respect to the eigenbasis of sigma, which is the matrix U.
+
+%%% YOUR CODE HERE %%%
+sigma = x * x' / size(x, 2);
+[U, S, V] = svd(sigma);
+
+%% Step 4a: Implement PCA with whitening and regularisation
+%  Implement PCA with whitening and regularisation to produce the matrix
+%  xPCAWhite. 
+xPCAwhite = diag(1./sqrt(diag(S) + epsilon)) * U' * x;
+
+%% Step 5: Implement ZCA whitening
+%  Now implement ZCA whitening to produce the matrix xZCAWhite. 
+%  Visualise the data and compare it to the raw data. You should observe
+%  that whitening results in, among other things, enhanced edges.
+
+%%% YOUR CODE HERE %%%
+Z = U * xPCAwhite;
